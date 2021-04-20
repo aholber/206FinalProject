b9e872fd1c6cfb6bc4d3f4aece4da6d3356b2668
aholber <aholber@umich.edu>
2021-04-20 00:55:35 -0400

    this isnt working

---
+++ nhl.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import os
+import csv
+import sqlite3
+import json
+
+#def get_player_information():
+url = "http://www.nhl.com/stats/skaters?reportType=season&seasonFrom=20162017&seasonTo=20202021&gameType=2&filter=gamesPlayed,gte,1&sort=points,goals,assists&page=0&pageSize=100"
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+
+ranking_list = []
+name_list = []
+points_list = []
+final_list = []
+
+#find all rankings, names, and points for players
+rankings = soup.find_all('div', class_='rt-td index-column rthfc-td-fixed rthfc-td-fixed-left')
+names = soup.find_all('div', class_= 'rt-td rthfc-td-fixed rthfc-td-fixed-left rthfc-td-fixed-left-last')
+points = soup.find_all('div', class_= 'rt-td primarySort')
+
+for rank in rankings:
+    ranking_list.append(rank.text.strip())
+for name in names:
+    name_list.append(name.text.strip())
+for point in points:
+    points_list.append(point.text.strip())
+
+for i in range(len(ranking_list)):
+    final_list.append((ranking_list[i], name_list[i], points_list[i]))
+
+print(final_list)
+
+
