b15c3132e88635405f78c5a299fa7a386a6fe3cc
aholber <aholber@umich.edu>
2021-04-25 14:40:49 -0400

    idek

--- combined.py
+++ combined.py
@@ -43,7 +43,7 @@ def player_info():
 def search(player):
     
     searchQuery = {
-        'access_key': 'e2b362106cc9821f8ebfdc35e48c265b',
+        'access_key': 'cffb9950e098f49f973ffb30e8f2274e',
         'query': player
     }
 
@@ -54,6 +54,10 @@ def search(player):
     #print(searchResults)
 
     birthmonth = searchResults['knowledge_graph']['known_attributes'][0]['value'][0:3]
+    if len(birthmonth) > 3:
+        birthmonth = birthmonth[0:3]
+    else:   
+        birthmonth = birthmonth
     
     info = searchResults['knowledge_graph']['known_attributes'][0]['value']
     info = info.split(',')
@@ -67,7 +71,56 @@ def search(player):
 
     return player, birthmonth, birthcountry
 
-for player in player_info():
-    print(search(player[0]))
+#
+#    print(search(player[0]))
 
-#earch()
+#search()
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+
+def setup_players_table(cur, conn):
+    data = player_info()
+    cur.execute('CREATE TABLE IF NOT EXISTS Players (name TEXT PRIMARY KEY, points INTEGER)')
+    for x in range(0,25):
+        cur.execute('SELECT * FROM Players')
+        rows = len(cur.fetchall())
+        cur.execute('INSERT INTO Players ( name, points) VALUES (?, ?)', (data[rows][0], data[rows][1]))
+    conn.commit()
+
+
+def setup_month_id(cur,conn):
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    cur.execute('CREATE TABLE IF NOT EXISTS Months_id (id INTEGER PRIMARY KEY, month TEXT)')  
+    for i in range(len(months)):
+        cur.execute('INSERT INTO Months_id (id, month) VALUES (?,?)', (i+1, months[i]))  
+    conn.commit()
+
+
+def setup_information_table(cur, conn):
+    for player in player_info():
+        data = search(player[0])
+    cur.execute('CREATE TABLE IF NOT EXISTS Information (name TEXT PRIMARY KEY, month TEXT, country TEXT)')
+    for x in range(0,25):
+        cur.execute('SELECT * FROM Information')
+        rows = len(cur.fetchall())
+        cur.execute('INSERT INTO Players (name, month, country) VALUES (?, ?, ?)', (data[rows][0], data[rows][1], data[rows][2]))
+    conn.commit()
+
+
+def main():
+    #player_info()
+    cur, conn = setUpDatabase('players.db')
+    setup_players_table(cur, conn)
+    setup_players_table(cur, conn)
+    setup_players_table(cur, conn)
+    setup_players_table(cur, conn)
+    setup_month_id(cur,conn)
+    setup_information_table(cur, conn)
+
+
+if __name__ == "__main__":
+    main()
